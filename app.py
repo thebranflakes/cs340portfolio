9bf32d5da65112fedf5b5977021f81ecc82acfe0
Brandon Hernandez <hernanb2@flip2.engr.oregonstate.edu>
2022-05-04 13:47:11 -0700

    think this is right?

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+import database.db_connector as db
 
 # Configuration
 
 app = Flask(__name__)
+db_connection = db.connect_to_database()
 
 # Routes 
 
+people_from_app_py = [
+{
+    "name": "Thomas",
+    "age": 33,
+    "location": "New Mexico",
+    "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
+}
+]
+
+
 @app.route('/')
 def root():
-    return "Welcome to the OSU CS 340 - Flask Tutorial!"
+    return render_template("main.j2")
 
+@app.route('/tickets_sold')
+def bsg_people():
+    return "This is the tickets_sold route."
 # Listener
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9112)) 
-    #                                 ^^^^
-    #              You can replace this number with any valid port
-    
-    app.run(port=port) 
+    port = int(os.environ.get('PORT', 9112))
+    app.run(port=port, debug=True)
